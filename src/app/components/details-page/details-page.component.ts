d1edaf6da6f1c5f488949cfa670c9673207fbcb2
Eric Sommerhalder <eric.sommerhalder@unibas.ch>
2020-06-29 17:27:16 +0200

    Details page recursive

--- src/app/components/details-page/details-page.component.ts
+++ src/app/components/details-page/details-page.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {KnoraService} from '../../services/knora.service';
+import {ReadLinkValue, ReadResource} from '@knora/api';
+import {first} from 'rxjs/operators';
 
+class PropStore {
+  constructor(public name: string, public values: string[], public links: PropStore[]) {
+  }
+}
 @Component({
   selector: 'app-details-page',
   templateUrl: './details-page.component.html',
@@ -9,18 +15,55 @@ import {KnoraService} from '../../services/knora.service';
 })
 export class DetailsPageComponent implements OnInit {
   iri: string;
+  props: PropStore[];
   constructor(public route: ActivatedRoute,
               private knoraService: KnoraService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.route.params.subscribe(params => {
       this.iri = params.iri;
-      this.knoraService.getResource(params.iri).subscribe(
-        res => {
-          console.log('=========>', res);
-        }
-      );
     });
+    this.props = await this.getAllData(this.iri);
+    console.log(this.props);
   }
+  async getAllData(iri: string) {
+    return await this.getAllDataHelper(iri);
+  }
+  async getAllDataHelper(iri: string): Promise<PropStore[]> {
+    const toReturn = [];
+    let needForDummies = false;
+    const resource = await this.knoraService.getResource(iri).pipe(first()).toPromise();
+    for (const i in resource.properties) {
+      const thisProp = new PropStore(resource.properties[i][0].propertyLabel, [], []);
+      if (resource.properties[i][0] instanceof ReadLinkValue && resource.properties[i].length > 1) { // multiple linked resources for same property
+        needForDummies = true;
+      }
+      for (const val of resource.properties[i]) {
+        if (needForDummies) {
+          const dummy = new PropStore('dummy', [], []);
+          const v: ReadLinkValue = val as ReadLinkValue;
+          dummy.links = await this.getAllDataHelper(v.linkedResourceIri);
+          thisProp.links.push(dummy);
+        } else {
+          if (val instanceof ReadLinkValue) {
+            thisProp.links = await this.getAllDataHelper(val.linkedResourceIri);
+          } else {
+            thisProp.values.push(val.strval);
+          }
+        }
+      }
+      needForDummies = false;
+      toReturn.push(thisProp);
+    }
+    return toReturn;
+    /*this.knoraService.getResource(iri).subscribe(
+      resource => {
+        const toReturn = new PropStore('', [], []);
+
+        return toReturn;
+      }
+    );*/
+  }
+
 
 }
